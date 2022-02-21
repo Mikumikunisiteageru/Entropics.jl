9ae04b942eb73fab954d28c0419505f45ea9eadb
Yang YC <yang.yc.allium@gmail.com>
2022-02-22 00:05:02 +0800

    Bug fixed

--- src/MaximumEntropy.jl
+++ src/MaximumEntropy.jl
@@ -43,7 +43,7 @@ me_u(min_, max_) = me_u(float(min_), float(max_))
 # Known min, max, median
 function me_up(min_::T, max_::T, median_::T) where {T <: AbstractFloat}
 	uniform_1 = me_u(min_, median_)
-	uniform_2 = me_u(medina_, max_, true)
+	uniform_2 = me_u(median_, max_, true)
 	x -> (uniform_1(x) + uniform_2(x)) / 2
 end
 me_up(min_, max_, median_) = me_up(float(min_), float(max_), float(median_))
@@ -54,7 +54,7 @@ function me_e(min_::T, max_::T, mean_::T) where {T <: AbstractFloat}
 	d = (max_ - min_) / 2
 	f(g) = d * cothminv(d * g) - t
 	gamma = find_zero(f, 0)
-	gamma < 1e-10 ||
+	abs(gamma) < 1e-10 &&
 		return me_u(min_, max_)
 	c = gamma / (exp(max_*gamma) - exp(min_*gamma))
 	x -> min_ <= x <= max_ ? c * exp(gamma * x) : 0
