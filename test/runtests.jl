ec69be0a864acabef65353ea35b4ac21b4581940
Yang YC <yang.yc.allium@gmail.com>
2022-02-25 11:11:00 +0800

    Update Project.toml

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,14 @@
+# runtests.jl
+# 20220225
+
 using MaximumEntropy
 using Test
+
+@test !isnothing(maxent(min=0, max=1))
+@test !isnothing(maxent(min=0, max=1, mean=0.5))
+@test !isnothing(maxent(min=0, max=1, mean=0.3))
+@test !isnothing(maxent(min=0, max=1, median=0.2))
+@test !isnothing(maxent(min=0, max=1, median=0.2, mean=0.5))
+@test !isnothing(maxent(min=0, max=1, median=0.2, mean=0.3))
+@test !isnothing(maxent(min=0, max=1, mean=0.5, std=0.1))
+@test !isnothing(maxent(min=0, max=1, mean=0.3, std=0.1))
