5497f3d0965991f8e60383678ee0f165636939b0
Yu-Chang YANG <yang.yc.allium@gmail.com>
2022-12-07 16:03:51 +0800

    v0.2.0, reconstructing types and syntax

--- src/Entropics.jl
+++ src/Entropics.jl
@@ -1,136 +1,417 @@
 module Entropics
 
-export maxent
+export maxendist, median, mean, var, entropy, PDF, CDF
 
-using Roots: find_zero
-using NLsolve: nlsolve
-using SpecialFunctions: erf
+using NLsolve: nlsolve, converged
 
-phi(x) = exp(-0.5 * x^2) / sqrt(2*pi)
-Phi(x) = 0.5 * (1 + erf(x/sqrt(2)))
-function cothminv(x)
-	abs(x) > 0.5 && return coth(x) - 1/x
-	x * evalpoly(x^2, 
-		[+3.3333333333333333e-01, 2.2222222222222223e-02,
-		 +2.1164021164021165e-03, 2.1164021164021165e-04,
-		 +2.1377799155576935e-05, 2.1644042808063972e-06,
-		 +2.1925947851873778e-07, 2.2214608789979678e-08,
-		 +2.2507846516808994e-09, 2.2805151204592183e-10,
-		 +2.3106432599002627e-11, 2.3411706819824886e-12,
-		 +2.3721017400233653e-13, 2.4034415333307705e-14,
-		 +2.4351954029183367e-15, 2.4673688045172075e-16,
-		 +2.4999672771220810e-17, 2.5329964357406350e-18,
-		 +2.5664619702826290e-19, 2.6003696460137274e-20])
+using SpecialFunctions: erf, erfi, erfinv
+
+# MATHEMATIC FUNCTIONS
+
+function erfiinv(x::T) where {T<:AbstractFloat}
+	iszero(x) && return x
+	x < 0 && return -erfiinv(-x)
+	function f!(output, param)
+		output[1] = erfi(param[1]) - x
+	end
+	return nlsolve(f!, [sqrt(log(1 + x))]).zero[1]
+end
+erfiinv(x::Real) = erfiinv(float(x))
+
+function cothminv(x::T) where {T<:AbstractFloat}
+	abs(x) > 0.5 && return coth(x) - inv(x)
+	return T(x * evalpoly(x^2, 
+		[3.3333333333333333e-01, 2.2222222222222223e-02,
+		 2.1164021164021165e-03, 2.1164021164021165e-04,
+		 2.1377799155576935e-05, 2.1644042808063972e-06,
+		 2.1925947851873778e-07, 2.2214608789979678e-08,
+		 2.2507846516808994e-09, 2.2805151204592183e-10,
+		 2.3106432599002627e-11, 2.3411706819824886e-12,
+		 2.3721017400233653e-13, 2.4034415333307705e-14,
+		 2.4351954029183367e-15, 2.4673688045172075e-16,
+		 2.4999672771220810e-17, 2.5329964357406350e-18,
+		 2.5664619702826290e-19, 2.6003696460137274e-20]))
+end
+cothminv(x::Real) = cothminv(float(x))
+
+abstract type MaxEnDist end
+
+struct PDF{T<:MaxEnDist} <: Function
+	d::T
 end
 
-# Known min, max
-function me_u(min_::T, max_::T, left_open=false) where {T <: AbstractFloat}
-	range_ = max_ - min_
-	isnan(range_) && error("Interval [$min_, $max_] has no meaning!")
-	isinf(range_) && error("Interval [$min_, $max_] has infinite length!")
-	iszero(range_) && error("Interval [$min_, $max_] has zero length!")
-	range_ < 0 && error("Interval [$min_, $max_] is reversed!")
-	if left_open
-		return x -> min_ < x <= max_ ?
-			one(T) / (max_ - min_) : zero(T)
-	else
-		return x -> min_ <= x <= max_ ?
-			one(T) / (max_ - min_) : zero(T)
+struct CDF{T<:MaxEnDist} <: Function
+	d::T
+end
+
+###
+
+struct MED000{T<:AbstractFloat} <: MaxEnDist
+	a::T
+	b::T
+end
+MED000(a::AbstractFloat, b::AbstractFloat) = MED000(promote(a, b)...)
+MED000(a::Real, b::Real) = MED000(float(a), float(b))
+median(d::MED000)  = (d.a + d.b) / 2
+mean(d::MED000)    = (d.a + d.b) / 2
+var(d::MED000)     = (d.b - d.a)^2 / 12
+entropy(d::MED000) = log(d.b - d.a)
+(p::PDF{MED000{T}})(x::Real) where {T<:AbstractFloat} = 
+	p.d.a <= x <= p.d.b ? inv(p.d.b-p.d.a) : zero(T)
+(P::CDF{MED000{T}})(x::Real) where {T<:AbstractFloat} = 
+	P.d.a <= x <= P.d.b ? (T(x)-P.d.a) / (P.d.b-P.d.a) : T(x > P.d.b)
+med(a::Real, b::Real, ::Nothing, ::Nothing, ::Nothing) = MED000(a, b)
+
+###
+
+struct MED100{T<:AbstractFloat} <: MaxEnDist
+	a::T
+	b::T
+	m::T
+end
+MED100(a::AbstractFloat, b::AbstractFloat, m::AbstractFloat) = 
+	MED100(promote(a, b, m)...)
+MED100(a::Real, b::Real, m::Real) = MED100(float(a), float(b), float(m))
+median(d::MED100)  = m
+mean(d::MED100)    = (a + 2m + b) / 4
+var(d::MED100)     = (5 * (d.b-d.a)^2 + 4 * (d.m-d.a) * (d.m-d.b)) / 48
+entropy(d::MED100) = log(4 * (d.m-d.a) * (d.b-d.m)) / 2
+(p::PDF{MED100{T}})(x::Real) where {T<:AbstractFloat} = 
+	p.d.a <= x <= p.d.m ? inv(p.d.m-p.d.a)/2 : 
+	p.d.m <  x <= p.d.b ? inv(p.d.b-p.d.m)/2 : zero(T)
+(P::CDF{MED100{T}})(x::Real) where {T<:AbstractFloat} = 
+	P.d.m <  x <= P.d.b ? (T(x)-P.d.m) / (2 * (P.d.b-P.d.m)) + one(T)/2 : 
+	P.d.a <= x <= P.d.m ? (T(x)-P.d.a) / (2 * (P.d.m-P.d.a)) : T(x > P.d.b)
+med(a::Real, b::Real, m::Real, ::Nothing, ::Nothing) = MED100(a, b, m)
+
+###
+
+struct MED010{T<:AbstractFloat} <: MaxEnDist
+	a::T
+	b::T
+	k::T
+	g::T
+	function MED010(a::T, b::T, g::T) where {T<:AbstractFloat}
+		k = g / (exp(b*g) - exp(a*g))
+		new{T}(a, b, k, g)
 	end
 end
-me_u(min_, max_) = me_u(float(min_), float(max_))
+MED010(a::AbstractFloat, b::AbstractFloat, g::AbstractFloat) = 
+	MED010(promote(a, b, g)...)
+MED010(a::Real, b::Real, g::Real) = MED010(float(a), float(b), float(g))
+median(d::MED010) = log(exp(d.a * d.g) + d.g / (2 * d.k)) / d.g
+mean(d::MED010) = 
+	(d.k * (d.b * exp(d.b * d.g) - d.a * exp(d.a * d.g)) - 1) / d.g
+var(d::MED010) = -(2 / d.g + mean(d)) * mean(d) + 
+	d.k / d.g * (d.b^2 * exp(d.b * d.g) - d.a^2 * exp(d.a * d.g))
+entropy(d::MED010) = -log(d.k) - d.g * mean(d)
+(p::PDF{MED010{T}})(x::Real) where {T<:AbstractFloat} = 
+	p.d.a <= x <= p.d.b ? p.d.k * exp(p.d.g * T(x)) : zero(T)
+(P::CDF{MED010{T}})(x::Real) where {T<:AbstractFloat} = 
+	P.d.a <= x <= P.d.b ? 
+		P.d.k / P.d.g * (exp(T(x) * P.d.g) - exp(P.d.a * P.d.g)) : T(x > P.d.b)
 
-# Known min, max, median
-function me_up(min_::T, max_::T, median_::T) where {T <: AbstractFloat}
-	uniform_1 = me_u(min_, median_)
-	uniform_2 = me_u(median_, max_, true)
-	x -> (uniform_1(x) + uniform_2(x)) / 2
-end
-me_up(min_, max_, median_) = me_up(float(min_), float(max_), float(median_))
-
-# Known min, max, mean
-function me_e(min_::T, max_::T, mean_::T) where {T <: AbstractFloat}
-	t = mean_ - (min_+max_)/2
-	d = (max_ - min_) / 2
-	f(g) = d * cothminv(d * g) - t
-	gamma = find_zero(f, 0)
-	abs(gamma) < 1e-10 &&
-		return me_u(min_, max_)
-	c = gamma / (exp(max_*gamma) - exp(min_*gamma))
-	x -> min_ <= x <= max_ ? c * exp(gamma * x) : 0
-end
-me_e(min_, max_, mean_) = me_e(float(min_), float(max_), float(mean_))
-
-# Known min, max, mean, median
-function me_ep(min_::T, max_::T, mean_::T, median_::T) where {T <: AbstractFloat}
-	t = 2*mean_ - median_ - (min_+max_)/2
-	d1 = (median_ - min_) / 2
-	d2 = (max_ - median_) / 2
-	f(g) = d1 * cothminv(d1 * g) + d2 * cothminv(d2 * g) - t
-	gamma = find_zero(f, 0)
-	gamma < 1e-10 ||
-		return me_up(min_, max_, median_)
-	c1 = gamma / (2 * (exp(median_*gamma) - exp(min_*gamma)))
-	c2 = gamma / (2 * (exp(max_*gamma) - exp(median_*gamma)))
-	x -> min_ <= x <= median_ ? c1 * exp(gamma * x) :
-		 median_ < x <= max_ ? c2 * exp(gamma * x) : 0
-end
-me_ep(min_, max_, mean_, median_) = 
-	me_ep(float(min_), float(max_), float(mean_), float(median_))
-
-# Known min, max, mean, std
-function me_n(min_::T, max_::T, mean_::T, std_::T) where {T <: AbstractFloat}
-	var_ = std_ ^ 2
-	function f!(F, p)
-		mu = p[1]
-		sigma = p[2]
-		alpha = (min_ - mu) / sigma
-		beta = (max_ - mu) / sigma
-		phi_alpha = phi(alpha)
-		phi_beta = phi(beta)
-		z = phi(beta) - phi(alpha)
-		Z = Phi(beta) - Phi(alpha)
-		Q = z / Z
-		F[1] = mu - sigma * Q - mean_
-		F[2] = sigma^2 * (1 + (alpha*phi_alpha - beta*phi_beta)/Z - Q^2) - var_
+function med(a::Real, b::Real, ::Nothing, u::Real, ::Nothing)
+	t = u - (a + b) / 2
+	d = (b - a) / 2
+	function f!(output, param)
+		output[1] = d * cothminv(d * param[1]) - t
 	end
-	mu, sigma = nlsolve(f!, [0., 1.]).zero
-	F = zeros(2)
-	f!(F, [mu, sigma])
-	all(abs.(F) .< 1e-8) || error("No solution!")
-	alpha = (min_ - mu) / sigma
-	beta = (max_ - mu) / sigma
-	Z = Phi(beta) - Phi(alpha)
-	# entropy = log(sigma*Z*sqrt(2*pi*exp(1))+(alpha*phi(alpha)-beta*phi(beta))/(2*Z))
-	# println(entropy)
-	x -> min_ <= x <= max_ ?
-		phi((x-mu)/sigma) / (sigma * Z) : 0
+	solution = nlsolve(f!, [3 * t / d^2], ftol=1e-12)
+	converged(solution) || @warn "Low precision!"
+	g = solution.zero[1]
+	return isapprox(g, 0, atol=1e-10) ? MED000(a, b) : MED010(a, b, g)
 end
-me_n(min_, max_, mean_, std_) = 
-	me_n(float(min_), float(max_), float(mean_), float(std_))
 
-function maxent(; min=0, max=150, mean=nothing, median=nothing, std=nothing)
-	arg_type = 0b111 - sum(isnothing.((mean, median, std)) .<< (2:-1:0))
-	if arg_type == 0
-		return me_u(min, max)
-	elseif arg_type == 1
-		# error("Argument `std` given with no `mean`!")
-		return me_u(min, max)
-	elseif arg_type == 2
-		return me_up(min, max, median)
-	elseif arg_type == 3
-		# error("Argument `std` given with no `mean`!")
-		return me_up(min, max, median)
-	elseif arg_type == 4
-		return me_e(min, max, mean)
-	elseif arg_type == 5
-		return me_n(min, max, mean, std)
-	elseif arg_type == 6
-		return me_ep(min, max, mean, median)
-	elseif arg_type == 7
-		# error("Too complicated!")
-		return me_ep(min, max, mean, median)
+###
+
+struct MED110{T<:AbstractFloat} <: MaxEnDist
+	a::T
+	b::T
+	m::T
+	ka::T
+	kb::T
+	g::T
+	function MED110(a::T, b::T, m::T, g::T) where {T<:AbstractFloat}
+		ka = g / (2 * (exp(m*g) - exp(a*g)))
+		kb = g / (2 * (exp(b*g) - exp(m*g)))
+		new{T}(a, b, m, ka, kb, g)
 	end
 end
+MED110(a::AbstractFloat, b::AbstractFloat, m::AbstractFloat, 
+	g::AbstractFloat) = MED110(promote(a, b, m, g)...)
+MED110(a::Real, b::Real, m::Real, g::Real) = 
+	MED110(float(a), float(b), float(m), float(g))
+median(d::MED110) = d.m
+mean(d::MED110) = 
+	(d.kb * (d.b * exp(d.b * d.g) - d.m * exp(d.m * d.g)) + 
+	 d.ka * (d.m * exp(d.m * d.g) - d.a * exp(d.a * d.g)) - 1) / d.g
+var(d::MED110) = -(2 / d.g + mean(d)) * mean(d) + 
+	(d.kb * (d.b^2 * exp(d.b * d.g) - d.m^2 * exp(d.m * d.g)) + 
+	 d.ka * (d.m^2 * exp(d.m * d.g) - d.a^2 * exp(d.a * d.g))) / d.g
+entropy(d::MED110) = -(log(d.ka) + log(d.kb)) / 2 - d.g * mean(d)
+(p::PDF{MED110{T}})(x::Real) where {T<:AbstractFloat} = 
+	p.d.a <= x <= p.d.m ? p.d.ka * exp(p.d.g * T(x)) : 
+	p.d.m <  x <= p.d.b ? p.d.kb * exp(p.d.g * T(x)) : zero(T)
+(P::CDF{MED110{T}})(x::Real) where {T<:AbstractFloat} = 
+	P.d.m <  x <= P.d.b ? 
+		P.d.kb / P.d.g * (exp(T(x) * P.d.g) - exp(P.d.m * P.d.g)) + one(T)/2 : 
+	P.d.a <= x <= P.d.m ? 
+		P.d.ka / P.d.g * (exp(T(x) * P.d.g) - exp(P.d.a * P.d.g)) : 
+			T(x > P.d.b)
 
-end # module
+function med(a::Real, b::Real, m::Real, u::Real, ::Nothing)
+	t = 2 * u - m - (a + b) / 2
+	da = (m - a) / 2
+	db = (b - m) / 2
+	function f!(output, param)
+		output[1] = da * cothminv(da * param[1]) + 
+			db * cothminv(db * param[1]) - t
+	end
+	solution = nlsolve(f!, [3 * t / (da^2 + db^2)], ftol=1e-12)
+	converged(solution) || @warn "Low precision!"
+	g = solution.zero[1]
+	return isapprox(g, 0, atol=1e-10) ? MED100(a, b, m) : MED110(a, b, m, g)
+end
+
+###
+
+struct MED011N{T<:AbstractFloat} <: MaxEnDist
+	a::T
+	b::T
+	k::T
+	o::T
+	s::T
+	t2::T
+	t3::T
+	mu::T
+	function MED011N(a::T, b::T, o::T, s::T) where {T<:AbstractFloat}
+		k = 2 / (sqrt(pi) * s * (erf((b-o)/s) - erf((a-o)/s)))
+		ea = exp(- ((a - o) / s) ^ 2)
+		eb = exp(- ((b - o) / s) ^ 2)
+		khalf = k / 2
+		t2 = khalf * (eb - ea)
+		t3 = khalf * (b * eb - a * ea)
+		mu = o - s^2 * t2
+		new{T}(a, b, k, o, s, t2, t3, mu)
+	end
+end
+MED011N(a::AbstractFloat, b::AbstractFloat, 
+	o::AbstractFloat, s::AbstractFloat) = MED011N(promote(a, b, o, s)...)
+MED011N(a::Real, b::Real, o::Real, s::Real) = 
+	MED011N(float(a), float(b), float(o), float(s))
+median(d::MED011N) = 
+	d.o + d.s * erfinv(erf((d.a-d.o)/d.s) + 1/(sqrt(pi)*d.k*d.s))
+mean(d::MED011N) = d.mu
+var(d::MED011N{T}) where {T<:AbstractFloat} = 
+	d.s^2 * (one(T)/2 - d.t3) + (d.o - d.mu) * d.mu
+entropy(d::MED011N) = (1 - 2*log(d.k)) / 2 + d.o * d.t2 - d.t3
+(p::PDF{MED011N{T}})(x::Real) where {T<:AbstractFloat} = 
+	p.d.a <= x <= p.d.b ? p.d.k * exp(-((T(x)-p.d.o)/p.d.s)^2) : zero(T)
+(P::CDF{MED011N{T}})(x::Real) where {T<:AbstractFloat} = 
+	P.d.a <= x <= P.d.b ? sqrt(pi)/2*P.d.k*P.d.s * 
+		(erf((T(x)-P.d.o)/P.d.s) - erf((P.d.a-P.d.o)/P.d.s)) : T(x > P.d.b)
+
+struct MED011P{T<:AbstractFloat} <: MaxEnDist
+	a::T
+	b::T
+	k::T
+	o::T
+	s::T
+	t2::T
+	t3::T
+	mu::T
+	function MED011P(a::T, b::T, o::T, s::T) where {T<:AbstractFloat}
+		k = 2 / (sqrt(pi) * s * (erfi((b-o)/s) - erfi((a-o)/s)))
+		ea = exp(((a - o) / s) ^ 2)
+		eb = exp(((b - o) / s) ^ 2)
+		khalf = k / 2
+		t2 = khalf * (eb - ea)
+		t3 = khalf * (b * eb - a * ea)
+		mu = o - s^2 * t2
+		new{T}(a, b, k, o, s, t2, t3, mu)
+	end
+end
+MED011P(a::AbstractFloat, b::AbstractFloat, 
+	o::AbstractFloat, s::AbstractFloat) = MED011P(promote(a, b, o, s)...)
+MED011P(a::Real, b::Real, o::Real, s::Real) = 
+	MED011P(float(a), float(b), float(o), float(s))
+median(d::MED011P) = 
+	d.o + d.s * erfiinv(erfi((d.a-d.o)/d.s) + 1/(sqrt(pi)*d.k*d.s))
+mean(d::MED011P) =  d.mu
+var(d::MED011P{T}) where {T<:AbstractFloat} = 
+	d.s^2 * (one(T)/-2 + d.t3) + (d.o - d.mu) * d.mu
+entropy(d::MED011P) = (1 - 2*log(d.k)) / 2 + d.o * d.t2 - d.t3
+(p::PDF{MED011P{T}})(x::Real) where {T<:AbstractFloat} = 
+	p.d.a <= x <= p.d.b ? p.d.k * exp(((T(x)-p.d.o)/p.d.s)^2) : zero(T)
+(P::CDF{MED011P{T}})(x::Real) where {T<:AbstractFloat} = 
+	P.d.a <= x <= P.d.b ? sqrt(pi)/2*P.d.k*P.d.s * 
+		(erfi((T(x)-P.d.o)/P.d.s) - erfi((P.d.a-P.d.o)/P.d.s)) : T(x > P.d.b)
+
+function med(a::Real, b::Real, ::Nothing, u::Real, v::Real)
+	med_010 = med(a, b, nothing, u, nothing)
+	var_010 = var(med_010)
+	if v == var_010
+		return med_010
+	elseif v < var_010
+		function f011n!(outputs, params)
+			d = MED011N(a, b, params[1], params[2])
+			outputs[1] = mean(d) - u
+			outputs[2] = var(d) - v
+		end
+		solution = nlsolve(f011n!, [u, sqrt(2*v)], ftol=1e-12)
+		converged(solution) || @warn "Low precision!"
+		return MED011N(a, b, solution.zero...)
+	else # v > var_010
+		function f011p!(outputs, params)
+			d = MED011P(a, b, params[1], params[2])
+			outputs[1] = mean(d) - u
+			outputs[2] = var(d) - v
+		end
+		solution = nlsolve(f011p!, [a+b-u, sqrt(2*v)], ftol=1e-12)
+		converged(solution) || @warn "Low precision!"
+		return MED011P(a, b, solution.zero...)
+	end
+end
+
+###
+
+med(a::Real, b::Real, ::Nothing, ::Nothing, v::Real) = 
+	med(a, b, nothing, (a+b)/2, v)
+
+###
+
+struct MED111N{T<:AbstractFloat} <: MaxEnDist
+	a::T
+	b::T
+	m::T
+	ka::T
+	kb::T
+	o::T
+	s::T
+	t2::T
+	t3::T
+	mu::T
+	function MED111N(a::T, b::T, m::T, o::T, s::T) where {T<:AbstractFloat}
+		ka = 1 / (sqrt(pi) * s * (erf((m-o)/s) - erf((a-o)/s)))
+		kb = 1 / (sqrt(pi) * s * (erf((b-o)/s) - erf((m-o)/s)))
+		ea = exp(- ((a - o) / s) ^ 2)
+		em = exp(- ((m - o) / s) ^ 2)
+		eb = exp(- ((b - o) / s) ^ 2)
+		kbmka = kb - ka
+		t2 = (kb * eb - kbmka * em - ka * ea) / 2
+		t3 = (kb * b * eb - kbmka * m * em - ka * a * ea) / 2
+		mu = o - s^2 * t2
+		new{T}(a, b, m, ka, kb, o, s, t2, t3, mu)
+	end
+end
+MED111N(a::AbstractFloat, b::AbstractFloat, 
+	m::AbstractFloat, o::AbstractFloat, s::AbstractFloat) = 
+		MED111N(promote(a, b, m, o, s)...)
+MED111N(a::Real, b::Real, m::Real, o::Real, s::Real) = 
+	MED111N(float(a), float(b), float(m), float(o), float(s))
+median(d::MED111N) = d.m
+mean(d::MED111N) = d.mu
+var(d::MED111N{T}) where {T<:AbstractFloat} = 
+	d.s^2 * (one(T)/2 - d.t3) + (d.o - d.mu) * d.mu
+entropy(d::MED111N) = (1 - log(d.ka) - log(d.kb)) / 2 + d.o * d.t2 - d.t3
+(p::PDF{MED111N{T}})(x::Real) where {T<:AbstractFloat} = 
+	p.d.a <= x <= p.d.m ? p.d.ka * exp(-((T(x)-p.d.o)/p.d.s)^2) : 
+	p.d.m <  x <= p.d.b ? p.d.kb * exp(-((T(x)-p.d.o)/p.d.s)^2) : zero(T)
+(P::CDF{MED111N{T}})(x::Real) where {T<:AbstractFloat} = 
+	P.d.m <  x <= P.d.b ? sqrt(pi)/2*P.d.kb*P.d.s * 
+		(erf((T(x)-P.d.o)/P.d.s) - erf((P.d.m-P.d.o)/P.d.s)) + one(T)/2 : 
+	P.d.a <= x <= P.d.m ? sqrt(pi)/2*P.d.ka*P.d.s * 
+		(erf((T(x)-P.d.o)/P.d.s) - erf((P.d.a-P.d.o)/P.d.s)) : T(x > P.d.b)
+
+struct MED111P{T<:AbstractFloat} <: MaxEnDist
+	a::T
+	b::T
+	m::T
+	ka::T
+	kb::T
+	o::T
+	s::T
+	t2::T
+	t3::T
+	mu::T
+	function MED111P(a::T, b::T, m::T, o::T, s::T) where {T<:AbstractFloat}
+		ka = 1 / (sqrt(pi) * s * (erfi((m-o)/s) - erfi((a-o)/s)))
+		kb = 1 / (sqrt(pi) * s * (erfi((b-o)/s) - erfi((m-o)/s)))
+		ea = exp(((a - o) / s) ^ 2)
+		em = exp(((m - o) / s) ^ 2)
+		eb = exp(((b - o) / s) ^ 2)
+		kbmka = kb - ka
+		t2 = (kb * eb - kbmka * em - ka * ea) / 2
+		t3 = (kb * b * eb - kbmka * m * em - ka * a * ea) / 2
+		mu = o + s^2 * t2
+		new{T}(a, b, m, ka, kb, o, s, t2, t3, mu)
+	end
+end
+MED111P(a::AbstractFloat, b::AbstractFloat, 
+	m::AbstractFloat, o::AbstractFloat, s::AbstractFloat) = 
+		MED111P(promote(a, b, m, o, s)...)
+MED111P(a::Real, b::Real, m::Real, o::Real, s::Real) = 
+	MED111P(float(a), float(b), float(m), float(o), float(s))
+median(d::MED111P) = d.m
+mean(d::MED111P) = d.mu
+var(d::MED111P{T}) where {T<:AbstractFloat} = 
+	d.s^2 * (one(T)/-2 + d.t3) + (d.o - d.mu) * d.mu
+entropy(d::MED111P) = (1 - log(d.ka) - log(d.kb)) / 2 + d.o * d.t2 - d.t3
+(p::PDF{MED111P{T}})(x::Real) where {T<:AbstractFloat} = 
+	p.d.a <= x <= p.d.m ? p.d.ka * exp(((T(x)-p.d.o)/p.d.s)^2) : 
+	p.d.m <  x <= p.d.b ? p.d.kb * exp(((T(x)-p.d.o)/p.d.s)^2) : zero(T)
+(P::CDF{MED111P{T}})(x::Real) where {T<:AbstractFloat} = 
+	P.d.m <  x <= P.d.b ? sqrt(pi)/2*P.d.kb*P.d.s * 
+		(erfi((T(x)-P.d.o)/P.d.s) - erfi((P.d.m-P.d.o)/P.d.s)) + one(T)/2 : 
+	P.d.a <= x <= P.d.m ? sqrt(pi)/2*P.d.ka*P.d.s * 
+		(erfi((T(x)-P.d.o)/P.d.s) - erfi((P.d.a-P.d.o)/P.d.s)) : T(x > P.d.b)
+
+function med(a::Real, b::Real, m::Real, u::Real, v::Real)
+	med_110 = med(a, b, m, u, nothing)
+	var_110 = var(med_110)
+	if v == var_110
+		return med_110
+	elseif v < var_110
+		function f111n!(outputs, params)
+			d = MED111N(a, b, m, params[1], params[2])
+			outputs[1] = mean(d) - u
+			outputs[2] = var(d) - v
+		end
+		solution = nlsolve(f111n!, [u, sqrt(2*v)], ftol=1e-12)
+		converged(solution) || @warn "Low precision!"
+		return MED111N(a, b, m, solution.zero...)
+	else # v > var_110
+		function f111p!(outputs, params)
+			d = MED111P(a, b, m, params[1], params[2])
+			outputs[1] = mean(d) - u
+			outputs[2] = var(d) - v
+		end
+		solution = nlsolve(f111p!, [a+b-u, sqrt(2*v)], ftol=1e-12)
+		converged(solution) || @warn "Low precision!"
+		return MED111P(a, b, m, solution.zero...)
+	end
+end
+
+###
+
+function xinterval(a::Real, b::Real)
+	(isnan(a) || isnan(b)) && throw(ArgumentError(
+		"The endpoints of the interval should be both real numbers!"))
+	(isinf(a) || isinf(b)) && throw(ArgumentError(
+		"Only finite intervals are acceptable!"))
+	a == b && throw(ArgumentError("The interval has zero length!"))
+	a > b && throw(ArgumentError("The interval has reversed endpoints!"))
+	return true
+end
+
+function maxendist(a::Real, b::Real; 
+		median::Union{Real,Nothing}=nothing, mean::Union{Real,Nothing}=nothing, 
+		std::Union{Real,Nothing}=nothing, var::Union{Real,Nothing}=nothing)
+	xinterval(a, b)
+	return med(a, b, median, mean, var)
+end
+
+###
+
+end # module Entropics
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,13 +2,20 @@
 # 20220225
 
 using Entropics
+
+using SpecialFunctions: erfi
+
 using Test
 
-@test !isnothing(maxent(min=0, max=1))
-@test !isnothing(maxent(min=0, max=1, mean=0.5))
-@test !isnothing(maxent(min=0, max=1, mean=0.3))
-@test !isnothing(maxent(min=0, max=1, median=0.2))
-@test !isnothing(maxent(min=0, max=1, median=0.2, mean=0.5))
-@test !isnothing(maxent(min=0, max=1, median=0.2, mean=0.3))
-@test !isnothing(maxent(min=0, max=1, mean=0.5, std=0.1))
-@test !isnothing(maxent(min=0, max=1, mean=0.3, std=0.1))
+@testset "MATHEMATIC FUNCTIONS" begin
+	
+	@testset "erfiinv" begin
+		for i = [-10, -1, -0.5, -0.1, 0, 0.1, 0.5, 1, 10, 100, 1000]
+			@test isapprox(i, erfi(Entropics.erfiinv(i)))
+		end
+		for i = Float32[-10, -1, -0.5, -0.1, 0, 0.1, 0.5, 1, 10, 100, 1000]
+			@test isapprox(i, erfi(Entropics.erfiinv(i)))
+		end
+	end
+	
+end
